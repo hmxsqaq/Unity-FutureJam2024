3303cd55f86ddec5aacac10cc57710b3a3c50f23
hmxs <hmxsqaq@gmail.com>
2024-11-30 23:03:28 +0800

    [feat] update booms

---
+++ Assets/Hmxs/Scripts/Drops/Bomb.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Hmxs.Scripts.Drops
+{
+    public abstract class Bomb : Drop
+    {
+        private readonly List<Drop> _dropsInScope = new();
+
+        protected bool HasExploded { get; private set; }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out Drop drop) && !_dropsInScope.Contains(drop) && drop != this)
+                _dropsInScope.Add(drop);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out Drop drop) && _dropsInScope.Contains(drop) && drop != this)
+                _dropsInScope.Remove(drop);
+        }
+
+        protected virtual void Trigger() => HasExploded = true;
+
+        protected void Explode()
+        {
+            // todo: play explosion sound
+            var toDestroy = _dropsInScope.Where(t => t != null).ToList();
+            foreach (var drop in toDestroy)
+            {
+                if (drop.TryGetComponent(out Bomb bomb) && !bomb.HasExploded)
+                    bomb.Trigger();
+                else
+                    Destroy(drop.gameObject);
+            }
+            _dropsInScope.Clear();
+            // todo: play explosion vfx
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Hmxs/Scripts/Drops/BombTime.cs
@@ -0,0 +1,28 @@
+﻿using Hmxs.Toolkit;
+using UnityEngine;
+
+namespace Hmxs.Scripts.Drops
+{
+    public class BombTime : Bomb
+    {
+        [SerializeField] private float explosionTime;
+
+        protected override void Start()
+        {
+            base.Start();
+            this.AttachTimer(
+                duration: explosionTime,
+                onComplete: Trigger,
+                onUpdate: t =>
+                {
+                    // todo: update explosion time
+                });
+        }
+
+        protected override void Trigger()
+        {
+            base.Trigger();
+            Explode();
+        }
+    }
+}
