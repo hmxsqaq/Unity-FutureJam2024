b9a077579e75c32fb482184df477b171a0331801
Pditine <hanfe666@outlook.com>
2024-12-01 23:26:33 +0800

    [feat]封面初步

---
+++ Assets/Pditine/Scripts/UI/Cover.cs
@@ -0,0 +1,26 @@
+﻿// // -------------------------------------------------
+// // Copyright@ LiJianhao
+// // Author : LiJianhao
+// // Date: 2024_12_1
+// // License: MIT
+// // -------------------------------------------------
+
+using System;
+using PurpleFlowerCore;
+using UnityEngine;
+
+namespace Pditine.Scripts.UI
+{
+    public class Cover : MonoBehaviour
+    {
+        [SerializeField] private AudioClip bgm;
+        private void Update()
+        {
+            AudioSystem.PlayBGM(bgm);
+            if (Input.anyKeyDown)
+            {
+                SceneSystem.LoadScene(1);
+            }
+        }
+    }
+}
