34e6efaf85adf42f0da0e0237645c348e0ced256
hmxs <hmxsqaq@gmail.com>
2024-12-02 00:31:57 +0800

    [feat] update

--- Assets/Hmxs/Scripts/Drops/BombTime.cs
+++ Assets/Hmxs/Scripts/Drops/BombTime.cs
@@ -7,19 +7,14 @@ namespace Hmxs.Scripts.Drops
     public class BombTime : Bomb
     {
         [SerializeField] private float explosionTime;
-        [SerializeField] private TextMeshProUGUI timerText;
 
         protected override void Start()
         {
             base.Start();
-            timerText.text = $"{explosionTime:00.00}";
             this.AttachTimer(
                 duration: explosionTime,
-                onComplete: Trigger,
-                onUpdate: t =>
-                {
-                    timerText.text = $"{(explosionTime - t):00.00}";
-                });
+                onComplete: Trigger
+                );
         }
 
         protected override void Trigger()
---
+++ Assets/Hmxs/Scripts/GamePlay/PlayerData.cs
@@ -0,0 +1,8 @@
+﻿namespace Hmxs.Scripts.GamePlay
+{
+    public static class PlayerData
+    {
+        public static int Blood { get; set; } = 6;
+        public static int MaxBlood { get; } = 6;
+    }
+}
